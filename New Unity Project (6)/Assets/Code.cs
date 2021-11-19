56b87338eb3f0db1a9aa5c28e09d1b7b88225d44
Shadowshay <ruijie27@gmail.com>
2021-11-19 13:15:42 +0800

    Updated Code for Singleton

--- New Unity Project (6)/Assets/Code.cs
+++ New Unity Project (6)/Assets/Code.cs
@@ -4,40 +4,87 @@ using UnityEngine;
 
 public class Code : MonoBehaviour
 {
+    public static Code Instance { get; private set; }
+
     public int cash = 1000;
     public int netWorth = 1000; 
     public int daysPassed = 0;
     public TMPro.TextMeshPro Date ;
 
     // number of unit of stocks for each stock pattern
-    int goodStock = 0;
-    int badStock = 0;
-    int averageStock = 0;
+    public int goodStock = 0;
+    public int badStock = 0;
+    public int averageStock = 0;
 
     // value of stock 
-    int goodStockValue = 0;
-    int badStockValue = 0;
-    int averageStockValue = 0;
+    public int goodStockValue = 0;
+    public int badStockValue = 0;
+    public int averageStockValue = 0;
+
+    // singleton that other classes can take variables from
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void nextDay()
     {
-
+        calculateEarnings();
         daysPassed += 1;
         Date.SetText("Days Passed: " + daysPassed);
     }
 
+
+    // function that updates networth;
     void calculateEarnings()
     {
         if (Random.Range(-1, 2) > 0)
         {
             int increase = Random.Range(1, 10) * 1 / 100 * goodStockValue;
-            goodStockValue += Random.Range(1, 10) * 1 / 100 * goodStockValue;
+            goodStockValue += increase;
             netWorth += increase * goodStock ;
         }
         else
         {
-            netWorth -= goodStockValue * goodStock * Random.Range(1, 10) * 1 / 100;
+            int decrease = - Random.Range(1, 10) * 1 / 100 * goodStockValue;
+            goodStockValue += decrease;
+            netWorth -= decrease * goodStock;
         }
+
+        if (Random.Range(-1, 2) > 0)
+        {
+            int increase = Random.Range(1, 10) * 1 / 100 * averageStockValue;
+            averageStockValue += increase;
+            netWorth += increase * averageStock;
+        }
+        else
+        {
+            int decrease = -Random.Range(1, 10) * 1 / 100 * averageStockValue;
+            averageStockValue += decrease;
+            netWorth -= decrease * averageStock;
+        }
+
+        if (Random.Range(-1, 2) > 0)
+        {
+            int increase = Random.Range(1, 10) * 1 / 100 * badStockValue;
+            badStockValue += increase;
+            netWorth += increase * badStock;
+        }
+        else
+        {
+            int decrease = -Random.Range(1, 10) * 1 / 100 * badStockValue;
+            badStockValue += decrease;
+            netWorth -= decrease * badStock;
+        }
+
     }
 
     // Update is called once per frame
